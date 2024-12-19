9571a715c4d12f432a01d02b141fafaa9233f39a
ganeshvarmav <ssganeshvarma@gmail.com>
2024-12-19 06:21:37 +0530

    First commit from Ganesh

---
+++ main.tf
@@ -0,0 +1,21 @@
+# Azure Provider Configuration
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+# Resource block for Azure Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resource-group"  # Name of the Resource Group
+  location = "East US"                 # Location (Azure Region)
+}
+
+# Variables
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
